62f29de84f18df5d7712ca9e66b168afbcc4d254
miermontoto <UO283319@uniovi.es>
2023-02-02 12:21:26 +0100

    PL, tema 2

---
+++ PL/argumentos.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    int i;
+    printf("Num de argumentos: %d\n", argc);
+    for(i = 0; i < argc; i++)
+        printf("Argumento %d: %s\n", i, argv[i]);
+    return 0;
+}
---
+++ PL/hola.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("Alejandro Rodríguez López");
+    printf("Alejandro Rodríguez López");
+    return 0;
+}
